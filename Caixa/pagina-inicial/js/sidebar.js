8ab0365ed07fc264071ba08d82a99aa5524c3d25
Luis Fernando A. Lima <90354100+LuisFernandoALima@users.noreply.github.com>
2023-03-26 17:48:21 -0300

    Termino da criação do Menu base

--- Caixa/pagina-inicial/js/sidebar.js
+++ Caixa/pagina-inicial/js/sidebar.js
@@ -1,15 +1,10 @@
-var options = document.querySelector('.opcoes');
+var options = document.querySelector('#options');
+var opções= document.querySelector(".opcoes")
 var optionsMenu = document.querySelector('.optionsMenu');
 
 
-options.addEventListener("click", (e)=>{
-    if(optionsMenu.classList.contains('able') == true){
-        optionsMenu.classList.remove ('able')
-        optionsMenu.classList.add('disable')
-        options.classList.toggle('opcoesAble')
-    }else{
-        optionsMenu.classList.add ('able')
-        optionsMenu.classList.remove('disable')
-        options.classList.toggle('opcoesAble')
-    }
+opções.addEventListener("click", (e)=>{
+        optionsMenu.classList.toggle('disable')
+        opções.classList.toggle('opcoesAble')
 })
+
